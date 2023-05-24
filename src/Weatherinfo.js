c75e81d4ca59832bf55c62808d9a79e043a519fb
misskatiemarple <misskatiemarple@gmail.com>
2023-05-24 20:19:49 +0900

    Added the date on the weather component

--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,17 +1,21 @@
 import React from "react";
+import FormattedDate from "./FormattedDate";
+import "bootstrap/dist/css/bootstrap.css";
+import "./Weatherinfo.css";
 
 export default function Weatherinfo(props) {
   return (
     <div className="Weatherinfo">
-      <h1>{props.data.city}</h1>
-      <ul>
-        <li>Last updated:</li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
       <div className="row">
         <div className="col-6">
+          <h1>{props.data.location}</h1>
+          <ul>
+            <li>
+              <FormattedDate date={props.data.date} />
+            </li>
+            <li className="text-capitalize mb-3">{props.data.description}</li>
+          </ul>
           <div className="d-flex todays-temperature">
-            <img src={props.data.icon} alt={props.data.description} />
             <div className="currentTemperature">
               <ul>
                 <li>Temperature: {Math.round(props.data.temperature)}</li>
@@ -19,28 +23,30 @@ export default function Weatherinfo(props) {
                   <a href="/">°C</a> | <a href="/">°F</a>
                 </span>
               </ul>
+              <ul>
+                <li>Humidity: {props.data.humidity}%</li>
+                <li>Wind Speed: {props.data.wind}km/h</li>
+              </ul>
             </div>
-            <ul>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind Speed: {props.data.wind}km/h</li>
-            </ul>
           </div>
         </div>
-      </div>
 
-      <div className="outfit col-6">
-        <div className="card">
-          <div className="card-header">
-            <p>
-              <span>
-                <li>Description: {props.data.description}</li>
-              </span>
-            </p>
-          </div>
-          <div className="card-body">
-            <p className="card-text">
-              It's Springtime, wear a dress and cardigan
-            </p>
+        <div className="outfit col-6">
+          <div className="card mt-3">
+            <div className="card-header">
+              <p>
+                <span>
+                  <li className="text-capitalize text-center">
+                    Description: {props.data.description}
+                  </li>
+                </span>
+              </p>
+            </div>
+            <div className="card-body">
+              <p className="card-text text-center">
+                It's Springtime, wear a dress and cardigan
+              </p>
+            </div>
           </div>
         </div>
       </div>
